1d93155017a53dd989c7f8e3f38b72527dd01a93
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2021-01-12 08:03:59 -0500

    creating chart, importing data

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,34 @@
 // @TODO: YOUR CODE HERE!
+//Smokers vs Income
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("data.csv").then(function(acsData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    acsData.forEach(function(data) {
+      data.smokes = +data.smokes;
+      data.income = +data.income;
+    });
