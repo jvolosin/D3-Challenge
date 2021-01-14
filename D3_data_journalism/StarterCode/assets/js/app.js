597491b7fb2603f2748ebafeeff819805ca473d6
GIS Warrior <28575212+jvolosin@users.noreply.github.com>
2021-01-14 08:02:34 -0500

    getting chart to load

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -7,7 +7,7 @@ var svgHeight = 500;
 var margin = {
   top: 20,
   right: 40,
-  bottom: 60,
+  bottom: 80,
   left: 100
 };
 
@@ -16,7 +16,7 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -25,7 +25,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("data.csv").then(function(acsData) {
+d3.csv("/D3_data_journalism/StarterCode/assets/data/data.csv").then(function(acsData) {
 
     // Step 1: Parse Data/Cast as numbers
     // ==============================
@@ -37,11 +37,12 @@ d3.csv("data.csv").then(function(acsData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(data, d => d.smokes)])
+      .domain([d3.min(acsData, d => d.smokes),
+      d3.max(acsData, d=> d.smokes)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.income)])
+      .domain([0, d3.max(acsData, d => d.income)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -53,15 +54,17 @@ d3.csv("data.csv").then(function(acsData) {
     // ==============================
     chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
+      .style("font-size", "18px")
       .call(bottomAxis);
 
     chartGroup.append("g")
+      .style("font-size", "18px")
       .call(leftAxis);
 
     // Step 5: Create Circles
     // ==============================
     var circlesGroup = chartGroup.selectAll("circle")
-      .data(data)
+      .data(acsData)
       .enter()
       .append("circle")
       .attr("cx", d => xLinearScale(d.smokes))
@@ -106,8 +109,8 @@ d3.csv("data.csv").then(function(acsData) {
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
         .attr("class", "axisText")
         .text("Income");
-        
 
-}).catch(function(error) {
-  console.log(error);
-});
+
+  }).catch(function(error) {
+    console.log(error);
+  });
